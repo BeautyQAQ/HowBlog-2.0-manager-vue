3cc148349bcab1678c4977890c3939ac138ad037
huangshen <1426887150@qq.com>
2020-04-16 16:57:19 +0800

    初始化项目，添加markdown编辑器

---
+++ src/views/article/articleRelease.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <mavon-editor ref="editor" v-model="doc" style="height: 650px" />
+  </div>
+</template>
+
+<script>
+import { mavonEditor } from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
+export default {
+  name: 'Create',
+  components: { mavonEditor },
+  data() {
+    return {
+      doc: ''
+    }
+  }
+}
+</script>
+<style>
+.mavonEditor {
+  height: 100%;
+  width: 100%;
+}
+</style>
