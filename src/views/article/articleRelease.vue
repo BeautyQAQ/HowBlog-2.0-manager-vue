5918f83a8e946556fae7e472dd5423d76e5076ca
huangshen <1426887150@qq.com>
2020-04-16 17:53:15 +0800

    修改发布文章界面

--- src/views/article/articleRelease.vue
+++ src/views/article/articleRelease.vue
@@ -1,26 +1,93 @@
 <template>
   <div>
-    <mavon-editor ref="editor" v-model="doc" style="height: 650px" />
+    <div>
+      <mavon-editor ref="editor" v-model="doc" style="height: 650px" />
+    </div>
+    <br>
+    <el-row>
+      <el-col :span="12">
+        标签：
+        <el-select v-model="value" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-col>
+      <el-col :span="12">
+        分类：
+        <el-select v-model="value" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          ></el-option>
+        </el-select>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        标题：
+        <el-input v-model="input" placeholder="请输入标题"></el-input>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="6" :offset="21">
+        <el-button type="primary">发布文章</el-button>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import { mavonEditor } from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
+import { mavonEditor } from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
 
 export default {
-  name: 'Create',
+  name: "Create",
   components: { mavonEditor },
   data() {
     return {
-      doc: ''
-    }
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕"
+        },
+        {
+          value: "选项2",
+          label: "双皮奶"
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎"
+        },
+        {
+          value: "选项4",
+          label: "龙须面"
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭"
+        }
+      ],
+      value: "",
+      input: "",
+      doc: ""
+    };
   }
-}
+};
 </script>
 <style>
-.mavonEditor {
-  height: 100%;
-  width: 100%;
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+.el-col {
+  border-radius: 4px;
 }
 </style>
